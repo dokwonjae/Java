eaa6f2422c4e988747833dbf78293b262acbb015
dokwonjae <dobear7@naver.com>
2023-09-21 17:44:32 +0900

    9/21

---
+++ AloneJava/src/main/java/chap07/verify/exam05/Penguin.java
@@ -0,0 +1,41 @@
+package chap07.verify.exam05;
+
+/**
+ * packageName : chap07.verify.exam05
+ * fileName : Penguin
+ * author : GGG
+ * date : 2023-09-21
+ * description :
+ * 요약 :
+ * <p>
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * —————————————————————————————
+ * 2023-09-21         GGG          최초 생성
+ */
+public class Penguin {
+    //    속성 정의
+    String name;
+    String habitat;
+    //    setter/getter : alt + Insert
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    //    함수 : showInfo()
+    void showInfo() {
+        System.out.println(this.name + " 은 " + this.habitat + " 살아요!!!!");
+    }
+}
---
+++ AloneJava/src/main/java/chap07/verify/exam06/Student.java
@@ -0,0 +1,30 @@
+package chap07.verify.exam06;
+
+/**
+ * packageName : chap07.verify.exam06
+ * fileName : Student
+ * author : GGG
+ * date : 2023-09-21
+ * description :
+ * 요약 :
+ * <p>
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * —————————————————————————————
+ * 2023-09-21         GGG          최초 생성
+ */
+public class Student extends Person{
+String id;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
