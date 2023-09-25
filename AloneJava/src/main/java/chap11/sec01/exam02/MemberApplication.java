2299bcd2083676a924ac01852407be2fc4476e78
dokwonjae <dobear7@naver.com>
2023-09-25 17:53:17 +0900

    9/25

---
+++ AloneJava/src/main/java/chap11/sec01/exam02/MemberApplication.java
@@ -0,0 +1,21 @@
+package chap11.sec01.exam02;
+
+/**
+ * packageName : chap11.sec01.exam02
+ * fileName : MemberApplication
+ * author : GGG
+ * date : 2023-09-25
+ * description :
+ * 요약 :
+ *      해쉬코드(hashCode) : 객체를 식별하는 유일한 정수값(번호 중복 없음)
+ * <p>
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * —————————————————————————————
+ * 2023-09-25         GGG          최초 생성
+ */
+public class MemberApplication {
+    public static void main(String[] args) {
+//        HashMap에서 테스트 :
+    }
+}
